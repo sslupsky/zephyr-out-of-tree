1b0ba5155452db9bc8a61bcdd6d68dbb0d4557dc
Steven Slupsky <sslupsky@gmail.com>
2020-03-24 15:44:17 -0600

    add board runner for jlink

--- board.cmake
+++ board.cmake
@@ -1,4 +1,11 @@
-# Copyright (c) 2017 Google LLC.
+# Copyright (c) 2020 Scanimetrics Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+# bossac only works if running west on Linux (see bossac.py runner)
+# include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
+
+board_runner_args(jlink "--device=atsamd21g18" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
+# Warning:  Openocd overwrites the bootloader, so only use openocd if not using a bootloader
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
