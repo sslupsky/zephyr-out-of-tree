c2514cda76439314c50ef14bc034170630d38895
Steven Slupsky <sslupsky@gmail.com>
2020-04-29 09:07:43 -0600

    led:  update initialization
    kernel stacks command lists 2 stacks when using for some
    reason.  Not sure if this is just cosmetic.
    However, if we define the thread name and create thread
    during init() only 1 stack is listed.
    
    Put led configuration data into one struct.
    
    Optimized stack size.

--- witap/witap_led.c
+++ witap/witap_led.c
@@ -12,7 +12,7 @@
 #include "witap_led.h"
 
 /* size of stack area used by each thread */
-#define LED_THREAD_STACKSIZE 512
+#define LED_THREAD_STACKSIZE 256
 
 /* scheduling priority used by each thread */
 #define LED_THREAD_PRIORITY 7
@@ -23,31 +23,33 @@
 RING_BUF_DECLARE(led_ring_buf, LED_RING_BUF_BYTES);
 
 LOG_MODULE_DECLARE(myApp);
+K_THREAD_STACK_DEFINE(led_stack, LED_THREAD_STACKSIZE);
 
-
-struct k_sem animation_sync;
-static struct k_mutex led_lock;
-static bool led_initialized = false;
+struct led_data_t {
+	struct k_thread led_thread;
+	struct k_sem animation_sync;
+	struct k_mutex led_lock;
 
 #if DT_HAS_ALIAS(led_red)
-static struct device *red_led_dev;
+	struct device *red_dev;
 #endif
 
 #if DT_HAS_ALIAS(led_green)
-static struct device *green_led_dev;
+	struct device *green_dev;
 #endif
 
 #if DT_HAS_ALIAS(led_blue)
-static struct device *blue_led_dev;
+	struct device *blue_dev;
 #endif
 
 #if DT_HAS_ALIAS(led_white)
-static struct device *white_led_dev;
+	struct device *white_dev;
 #endif
 
 #if DT_HAS_ALIAS(led_mono)
-static struct device *mono_led_dev;
+	struct device *mono_dev;
 #endif
+};
 
 const struct led_color_t LED_COLOR_BLACK = {
 	.red = 0,
@@ -124,74 +126,25 @@ const struct led_animation_t animation_error = {
 	.repeat = 2,
 };
 
-void led_init()
-{
-	/* Set LED pin as output */
-#if DT_HAS_ALIAS(led_red)
-	red_led_dev = device_get_binding(LED_RED_DEVICE_NAME);
-	if (!red_led_dev) {
-		LOG_ERR("LED RED device driver not found");
-		return;
-	}
-	gpio_pin_configure(red_led_dev, LED_RED, GPIO_OUTPUT);
-#endif
-
-#if DT_HAS_ALIAS(led_green)
-	green_led_dev = device_get_binding(LED_GREEN_DEVICE_NAME);
-	if (!green_led_dev) {
-		LOG_ERR("LED Green device driver not found");
-		return;
-	}
-	gpio_pin_configure(green_led_dev, LED_GREEN, GPIO_OUTPUT);
-#endif
-
-#if DT_HAS_ALIAS(led_blue)
-	blue_led_dev = device_get_binding(LED_BLUE_DEVICE_NAME);
-	if (!blue_led_dev) {
-		LOG_ERR("LED Blue device driver not found");
-		return;
-	}
-	gpio_pin_configure(blue_led_dev, LED_BLUE, GPIO_OUTPUT);
-#endif
-
-#if DT_HAS_ALIAS(led_white)
-	white_led_dev = device_get_binding(LED_WHITE_DEVICE_NAME);
-	if (!white_led_dev) {
-		LOG_ERR("LED White device driver not found");
-		return;
-	}
-	gpio_pin_configure(white_led_dev, LED_WHITE, GPIO_OUTPUT);
-#endif
-
-#if DT_HAS_ALIAS(led_mono)
-	mono_led_dev = device_get_binding(LED_MONO_DEVICE_NAME);
-	if (!mono_led_dev) {
-		LOG_ERR("LED Mono device driver not found");
-		return;
-	}
-	gpio_pin_configure(mono_led_dev, LED_MONO, GPIO_OUTPUT);
-#endif
-
-   	k_sem_init(&animation_sync, 0, LED_MAX_ANIMATIONS);
-	led_initialized = true;
-}
+static bool led_initialized = false;
+static struct led_data_t led_data;
 
 void led_set_color(const struct led_color_t *color)
 {
 #if DT_HAS_ALIAS(led_red)
-		gpio_pin_set(red_led_dev, LED_RED, color->red);
+		gpio_pin_set(led_data.red_dev, LED_RED, color->red);
 #endif
 #if DT_HAS_ALIAS(led_green)
-		gpio_pin_set(green_led_dev, LED_GREEN, color->green);
+		gpio_pin_set(led_data.green_dev, LED_GREEN, color->green);
 #endif
 #if DT_HAS_ALIAS(led_blue)
-		gpio_pin_set(blue_led_dev, LED_BLUE, color->blue);
+		gpio_pin_set(led_data.blue_dev, LED_BLUE, color->blue);
 #endif
 #if DT_HAS_ALIAS(led_white)
-		gpio_pin_set(white_led_dev, LED_WHITE, color->white);
+		gpio_pin_set(led_data.white_dev, LED_WHITE, color->white);
 #endif
 #if DT_HAS_ALIAS(led_mono)
-		gpio_pin_set(mono_led_dev, LED_MONO, color->red |color->green
+		gpio_pin_set(led_data.mono_dev, LED_MONO, color->red |color->green
 			     | color->blue | color->white);
 #endif
 }
@@ -225,7 +178,7 @@ void led_process(void *p1, void *p2, void *p3)
 	}
 
 	while (1) {
-		k_sem_take(&animation_sync, K_FOREVER);
+		k_sem_take(&led_data.animation_sync, K_FOREVER);
 		ret = ring_buf_get(&led_ring_buf, (u8_t *) &animation, sizeof(animation));
 		if (ret == sizeof(animation)) {
 			switch ( animation.effect ) {
@@ -252,19 +205,76 @@ void led_add(const struct led_animation_t *animation) {
 		return;
 	}
 
-	k_mutex_lock(&led_lock, K_FOREVER);
-	if (k_sem_count_get(&animation_sync) < LED_MAX_ANIMATIONS) {
+	k_mutex_lock(&led_data.led_lock, K_FOREVER);
+	if (k_sem_count_get(&led_data.animation_sync) < LED_MAX_ANIMATIONS) {
 		ret = ring_buf_put(&led_ring_buf, (u8_t *) animation, sizeof(struct led_animation_t));
 		if ( ret != sizeof(struct led_animation_t) ) {
 			LOG_ERR("LED ring buffer overflow");
 			goto done;
 		}
-		k_sem_give(&animation_sync);
+		k_sem_give(&led_data.animation_sync);
 	}
 done:
-	k_mutex_unlock(&led_lock);
+	k_mutex_unlock(&led_data.led_lock);
 	return;
 }
 
-K_THREAD_DEFINE(led, LED_THREAD_STACKSIZE, led_process, NULL, NULL, NULL,
-		LED_THREAD_PRIORITY, 0, 0);
+void led_init()
+{
+	/* Set LED pin as output */
+#if DT_HAS_ALIAS(led_red)
+	led_data.red_dev = device_get_binding(LED_RED_DEVICE_NAME);
+	if (!led_data.red_dev) {
+		LOG_ERR("LED RED device driver not found");
+		return;
+	}
+	gpio_pin_configure(led_data.red_dev, LED_RED, GPIO_OUTPUT);
+#endif
+
+#if DT_HAS_ALIAS(led_green)
+	led_data.green_dev = device_get_binding(LED_GREEN_DEVICE_NAME);
+	if (!led_data.green_dev) {
+		LOG_ERR("LED Green device driver not found");
+		return;
+	}
+	gpio_pin_configure(led_data.green_dev, LED_GREEN, GPIO_OUTPUT);
+#endif
+
+#if DT_HAS_ALIAS(led_blue)
+	led_data.blue_dev = device_get_binding(LED_BLUE_DEVICE_NAME);
+	if (!led_data.blue_dev) {
+		LOG_ERR("LED Blue device driver not found");
+		return;
+	}
+	gpio_pin_configure(led_data.blue_dev, LED_BLUE, GPIO_OUTPUT);
+#endif
+
+#if DT_HAS_ALIAS(led_white)
+	led_data.white_dev = device_get_binding(LED_WHITE_DEVICE_NAME);
+	if (!led_data.white_dev) {
+		LOG_ERR("LED White device driver not found");
+		return;
+	}
+	gpio_pin_configure(led_data.white_dev, LED_WHITE, GPIO_OUTPUT);
+#endif
+
+#if DT_HAS_ALIAS(led_mono)
+	led_data.mono_dev = device_get_binding(LED_MONO_DEVICE_NAME);
+	if (!led_data.mono_dev) {
+		LOG_ERR("LED Mono device driver not found");
+		return;
+	}
+	gpio_pin_configure(led_data.mono_dev, LED_MONO, GPIO_OUTPUT);
+#endif
+
+	k_thread_create(&led_data.led_thread, led_stack,
+			K_THREAD_STACK_SIZEOF(led_stack),
+			(k_thread_entry_t)led_process,
+			&led_data, NULL, NULL,
+			LED_THREAD_PRIORITY,
+			0, K_NO_WAIT);
+	k_thread_name_set(&led_data.led_thread, "led process");
+
+	k_sem_init(&led_data.animation_sync, 0, LED_MAX_ANIMATIONS);
+	led_initialized = true;
+}
