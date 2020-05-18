97bfab77fa96bdfd2a65c62856b875ecf4af6608
Steven Slupsky <sslupsky@gmail.com>
2020-05-18 10:16:07 -0600

    drivers: spi_nand: change timeout delays
    I was observing that the nand chip frequenty timed out
    during device operations (read, write and erase).
    
    This commit changes the timeouts an order of magnitude
    to 100ms each.
    
    Initial observation so far is that timeouts are infrequent now.
    I have not observed a single timeout in several days of
    testing.

--- drivers/flash/spi_nand.h
+++ drivers/flash/spi_nand.h
@@ -71,10 +71,10 @@ struct spi_nand_config {
 #define SPI_NAND_PAGE_ADDR_SIZE		1
 #define SPI_NAND_FEATURE_ADDR_SIZE 	1
 
-#define SPI_NAND_ERASE_TIMEOUT		14000	/* erase time out (us)   */
-#define SPI_NAND_PROG_TIMEOUT		1200	/* program time out (us) */
-#define SPI_NAND_READ_TIMEOUT		560	/* read time out (us) */
-#define SPI_NAND_RESET_TIMEOUT		20000	/* reset time out */
+#define SPI_NAND_ERASE_TIMEOUT		100000	/* erase time out (us)   */
+#define SPI_NAND_PROG_TIMEOUT		100000	/* program time out (us) */
+#define SPI_NAND_READ_TIMEOUT		100000	/* read time out (us) */
+#define SPI_NAND_RESET_TIMEOUT		100000	/* reset time out */
 
 #define SPI_NAND_MIN_ERASE_TIME 	1000    /* min erase time (us) */
 #define SPI_NAND_MIN_PROG_TIME		225     /* min programming time (us) */
