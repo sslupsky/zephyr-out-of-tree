e3bb54ba2359658ae1cd47a53bb11b883d67b54c
Steven Slupsky <sslupsky@gmail.com>
2020-07-04 10:23:22 -0600

    add bossac and jlink runners

--- boards/arm/arduino_mkr_1310/board.cmake
+++ boards/arm/arduino_mkr_1310/board.cmake
@@ -1,4 +1,11 @@
-# Copyright (c) 2017 Google LLC.
+# Copyright (c) 2020 Scanimetrics Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+# update bossac.py to run on macOS
+include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
+
+board_runner_args(jlink "--device=atsamd21g18" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
+# Warning:  Openocd overwrites the bootloader, so only use openocd if not using a bootloader
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
