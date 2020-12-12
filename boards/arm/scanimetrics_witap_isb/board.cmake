53d4d0fc8e3c11bc5863af388dd24cd4483796fc
Steven Slupsky <sslupsky@gmail.com>
2020-12-12 09:44:18 -0700

    migrate to samd21j18a
    Change pin assignments and migrate to new pcb
    design that uses SAMD21J18A mcu package (64 pins).

--- boards/arm/scanimetrics_witap_isb/board.cmake
+++ boards/arm/scanimetrics_witap_isb/board.cmake
@@ -4,7 +4,7 @@
 # bossac only works if running west on Linux (see bossac.py runner)
 # include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
 
-board_runner_args(jlink "--device=atsamd21g18" "--speed=4000")
+board_runner_args(jlink "--device=atsamd21j18" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 
 # Warning:  Openocd overwrites the bootloader, so only use openocd if not using a bootloader
--- boards/arm/scanimetrics_witap_isb/pinmux.c
+++ boards/arm/scanimetrics_witap_isb/pinmux.c
@@ -6,76 +6,82 @@
 
 #include <init.h>
 #include <drivers/pinmux.h>
+#include <soc.h>
 
 static int board_pinmux_init(struct device *dev)
 {
-	struct device __attribute__((unused)) *muxa = device_get_binding(DT_ATMEL_SAM0_PINMUX_PINMUX_A_LABEL);
-	struct device __attribute__((unused)) *muxb = device_get_binding(DT_ATMEL_SAM0_PINMUX_PINMUX_B_LABEL);
+	struct device __attribute__((unused)) *muxa = device_get_binding(DT_LABEL(DT_NODELABEL(pinmux_a)));
+	struct device __attribute__((unused)) *muxb = device_get_binding(DT_LABEL(DT_NODELABEL(pinmux_b)));
 
 	ARG_UNUSED(dev);
 
-#if DT_ATMEL_SAM0_UART_SERCOM_0_BASE_ADDRESS
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(0, atmel_sam0_uart) && CONFIG_UART_SAM0)
 #error Pin mapping is not configured
 #endif
-#if DT_ATMEL_SAM0_UART_SERCOM_1_BASE_ADDRESS
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(1, atmel_sam0_uart) && CONFIG_UART_SAM0)
 #error Pin mapping is not configured
 #endif
-#if DT_ATMEL_SAM0_UART_SERCOM_2_BASE_ADDRESS
-//  virtual sercom uart for console and shell via usb
-//  these pins are not mapped to external pins  since this port is virtual
-//  also note, the default pins (PA12 and PA13) are not connected on the ISB board.
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(2, atmel_sam0_uart) && CONFIG_UART_SAM0)
+#error Pin mapping is not configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(3, atmel_sam0_uart) && CONFIG_UART_SAM0)
+// 	pinmux_pin_set(muxa, 22, PINMUX_FUNC_C);
+// 	pinmux_pin_set(muxa, 23, PINMUX_FUNC_C);
 // #error Pin mapping is not configured
 #endif
-#if DT_ATMEL_SAM0_UART_SERCOM_3_BASE_ADDRESS
-#error Pin mapping is not configured
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(4, atmel_sam0_uart) && CONFIG_UART_SAM0)
+//  zephyr console uart (also used for usb_cdc)
+//  the pins are not presently connected on the ISB board.
+	// pinmux_pin_set(muxb, 12, PINMUX_FUNC_C);
+	// pinmux_pin_set(muxb, 13, PINMUX_FUNC_C);
+	// pinmux_pin_set(muxb, 12, PINMUX_FUNC_C);
+	// pinmux_pin_set(muxb, 13, PINMUX_FUNC_C);
+// #error Pin mapping is not configured
 #endif
-#if DT_ATMEL_SAM0_UART_SERCOM_4_BASE_ADDRESS
-#error Pin mapping is not configured
-#endif
-#if DT_ATMEL_SAM0_UART_SERCOM_5_BASE_ADDRESS
-#error Pin mapping is not configured
-#endif
-
-
-#if DT_ATMEL_SAM0_SPI_SERCOM_0_BASE_ADDRESS
-#error Pin mapping is not configured
-#endif
-#if DT_ATMEL_SAM0_SPI_SERCOM_1_BASE_ADDRESS
-#error Pin mapping is not configured
-#endif
-#if DT_ATMEL_SAM0_SPI_SERCOM_2_BASE_ADDRESS
-#error Pin mapping is not configured
-#endif
-#if DT_ATMEL_SAM0_SPI_SERCOM_3_BASE_ADDRESS
-#error Pin mapping is not configured
-#endif
-#if DT_ATMEL_SAM0_SPI_SERCOM_4_BASE_ADDRESS
-#error Pin mapping is not configured
-#endif
-#if DT_ATMEL_SAM0_SPI_SERCOM_5_BASE_ADDRESS
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(5, atmel_sam0_uart) && CONFIG_UART_SAM0)
 #error Pin mapping is not configured
 #endif
 
-#if DT_ATMEL_SAM0_I2C_SERCOM_0_BASE_ADDRESS
+
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(0, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+#error Pin mapping is not configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(1, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+#error Pin mapping is not configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(2, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+#error Pin mapping is not configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(3, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+#error Pin mapping is not configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(4, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+#error Pin mapping is not configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(5, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+#error Pin mapping is not configured
+#endif
+
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(0, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
 	/* SERCOM0 on SDA=PA08, SCL=PA09 */
 	pinmux_pin_set(muxa, 8, PINMUX_FUNC_C);
 	pinmux_pin_set(muxa, 9, PINMUX_FUNC_C);
 #endif
-#if DT_ATMEL_SAM0_I2C_SERCOM_1_BASE_ADDRESS
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(1, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
+	pinmux_pin_set(muxa, 16, PINMUX_FUNC_C);
+	pinmux_pin_set(muxa, 17, PINMUX_FUNC_C);
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(2, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
+	pinmux_pin_set(muxa, 12, PINMUX_FUNC_C);
+	pinmux_pin_set(muxa, 13, PINMUX_FUNC_C);
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(3, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
 #error Pin mapping is not configured
 #endif
-#if DT_ATMEL_SAM0_I2C_SERCOM_2_BASE_ADDRESS
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(4, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
 #error Pin mapping is not configured
 #endif
-#if DT_ATMEL_SAM0_I2C_SERCOM_3_BASE_ADDRESS
-	/* SERCOM0 on SDA=PA22, SCL=PA23 */
-	pinmux_pin_set(muxa, 22, PINMUX_FUNC_C);
-	pinmux_pin_set(muxa, 23, PINMUX_FUNC_C);
-#endif
-#if DT_ATMEL_SAM0_I2C_SERCOM_4_BASE_ADDRESS
-#error Pin mapping is not configured
-#endif
-#if DT_ATMEL_SAM0_I2C_SERCOM_5_BASE_ADDRESS
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(5, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
 	/* SERCOM0 on SDA=PB02, SCL=PB03 */
 	pinmux_pin_set(muxb, 2, PINMUX_FUNC_D);
 	pinmux_pin_set(muxb, 3, PINMUX_FUNC_D);
