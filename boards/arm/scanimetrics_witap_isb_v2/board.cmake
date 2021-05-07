301122da65254dd1e1fafc49a434ce943d339503
Steven Slupsky <sslupsky@gmail.com>
2021-05-07 11:00:38 -0600

    cmake:  add runner for bossac
    Add bossa runner
    Fix jlink runner (invalid cli option)

--- boards/arm/scanimetrics_witap_isb_v2/board.cmake
+++ boards/arm/scanimetrics_witap_isb_v2/board.cmake
@@ -1,10 +1,10 @@
 # Copyright (c) 2020 Scanimetrics Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-# bossac only works if running west on Linux (see bossac.py runner)
-# include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
+# added support for macOS in bossac runner (see bossac.py runner)
+include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
 
-board_runner_args(jlink "--device=atsamd51j20" "--speed=4000" "--offset=0x4000")
+board_runner_args(jlink "--device=atsamd51j20" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 
 # Warning:  Openocd overwrites the bootloader, so only use openocd if not using a bootloader
