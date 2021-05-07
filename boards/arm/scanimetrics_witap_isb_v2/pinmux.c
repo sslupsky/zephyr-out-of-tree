ff7eaf3baeb148825119faa0cfed57a02af33ae2
Steven Slupsky <sslupsky@gmail.com>
2021-05-07 10:55:16 -0600

    pinmux:  Update SAMD5x pins
    SAMD5x uses PINMUX_FUNC_H for USB pins
    SERCOM4 is used in rev 2036b and later hw revs.

--- boards/arm/scanimetrics_witap_isb_v2/pinmux.c
+++ boards/arm/scanimetrics_witap_isb_v2/pinmux.c
@@ -10,7 +10,7 @@
 
 static int board_pinmux_init(struct device *dev)
 {
-	struct device *muxa = device_get_binding(DT_LABEL(DT_NODELABEL(pinmux_a));
+	struct device *muxa = device_get_binding(DT_LABEL(DT_NODELABEL(pinmux_a)));
 	struct device *muxb = device_get_binding(DT_LABEL(DT_NODELABEL(pinmux_b)));
 	struct device *muxc = device_get_binding(DT_LABEL(DT_NODELABEL(pinmux_c)));
 	struct device *muxd = device_get_binding(DT_LABEL(DT_NODELABEL(pinmux_d)));
@@ -37,9 +37,7 @@ static int board_pinmux_init(struct device *dev)
 #endif
 #if (ATMEL_SAM0_DT_SERCOM_CHECK(4, atmel_sam0_uart) && CONFIG_UART_SAM0)
 //  zephyr console uart (also used for usb_cdc)
-//  the pins are not presently connected on the ISB board.
-	// pinmux_pin_set(muxb, 12, PINMUX_FUNC_C);
-	// pinmux_pin_set(muxb, 13, PINMUX_FUNC_C);
+//  SERCOM4 is connected on ISB Rev 2036C.
 	// pinmux_pin_set(muxb, 12, PINMUX_FUNC_C);
 	// pinmux_pin_set(muxb, 13, PINMUX_FUNC_C);
 // #error Pin mapping is not configured
@@ -88,15 +86,15 @@ static int board_pinmux_init(struct device *dev)
 #error Pin mapping is not configured
 #endif
 #if (ATMEL_SAM0_DT_SERCOM_CHECK(5, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
-	/* SERCOM0 on SDA=PB02, SCL=PB03 */
+	/* SERCOM5 on SDA=PB02, SCL=PB03 */
 	pinmux_pin_set(muxb, 2, PINMUX_FUNC_D);
 	pinmux_pin_set(muxb, 3, PINMUX_FUNC_D);
 #endif
 
 #ifdef CONFIG_USB_DC_SAM0
 	/* USB DP on PA25, USB DM on PA24 */
-	pinmux_pin_set(muxa, 25, PINMUX_FUNC_G);
-	pinmux_pin_set(muxa, 24, PINMUX_FUNC_G);
+	pinmux_pin_set(muxa, 25, PINMUX_FUNC_H);
+	pinmux_pin_set(muxa, 24, PINMUX_FUNC_H);
 #endif
 
 	return 0;
