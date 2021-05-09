78bf7cf9743330e3a876dc6bf255d081ebc0afa1
Steven Slupsky <sslupsky@gmail.com>
2021-05-09 12:00:35 -0600

    drivers: murata lora modem secrets
    Temporarily store secrets in the tree.
    In the future, these should probably be stored in the settings on the
    device.

---
+++ drivers/modem/secret.h
@@ -0,0 +1,16 @@
+/**
+ * @file secret.h
+ * @author Steven Slupsky (sslupsky@scanimetrics.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-04-03
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ * FIXME:  Temporarily store secrets here.
+ *         Create Kconfig options for setting these
+ */
+
+#define SECRET_APP_EUI "8E24353654DB80F0"
+#define SECRET_APP_KEY "BC07F5BF93324B4D9EDF786DD9DBB559"
+
