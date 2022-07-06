9492e7c73e0baddc0bf246ea97ae7cca3d75c99a
Steven Slupsky <sslupsky@gmail.com>
2022-07-05 21:05:00 -0600

    gnss:  Add Apache license to header

--- drivers/sensor/ublox-m8/shell_gnss.c
+++ drivers/sensor/ublox-m8/shell_gnss.c
@@ -1,11 +1,12 @@
 /**
- * @file Untitled-1
+ * @file shell_gnss.c
  * @author Steven Slupsky (sslupsky@scanimetrics.com)
  * @brief 
  * @version 0.1
  * @date 2020-05-26
  * 
  * @copyright Copyright (c) 2020
+ * SPDX-License-Identifier: Apache-2.0
  * 
  *    _____                 _                _        _          
  *   / ____|               (_)              | |      (_)         
